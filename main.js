9ae2223507c8b9e733bcf480487a351a158934cc
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-11-05 11:48:24 +0400

    Some Changes of logic

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ hamburger.addEventListener('click', () => {
 let btn = document.querySelector('.btn');
 let div = document.querySelector('.divv')
 
+
 btn.addEventListener('click', () => {
       
     if(inp.value !== '') {
@@ -41,10 +42,10 @@ btn.addEventListener('click', () => {
         let pattern= /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i
 
         if(pattern.test(email)) {
-           div.innerHTML('Request sent successfully')
+           div.innerHTML = 'Request sent successfully'
            console.log(inp.value);
         }  else {
-        alert('Please enter a valid email')
+           div.innerHTML ='Please enter a valid email!'
     }
     }
 })
